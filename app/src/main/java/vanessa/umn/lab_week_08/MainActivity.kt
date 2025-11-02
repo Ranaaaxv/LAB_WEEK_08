c458682269dae79dcb62ffa2ddf9813b3979e4e2
Ranaaaxv <vanessaaudrelia@gmail.com>
2025-11-02 22:30:39 +0700

    Commit No. 2 – add first foreground service

--- app/src/main/java/vanessa/umn/lab_week_08/MainActivity.kt
+++ app/src/main/java/vanessa/umn/lab_week_08/MainActivity.kt
@@ -1,5 +1,7 @@
 package vanessa.umn.lab_week_08
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -13,6 +15,9 @@ import androidx.work.Data
 import androidx.work.Constraints
 import vanessa.umn.lab_week_08.worker.FirstWorker
 import vanessa.umn.lab_week_08.worker.SecondWorker
+import android.content.Intent
+import androidx.core.content.ContextCompat
+
 class MainActivity : AppCompatActivity() {
     //Create an instance of a work manager
     //Work manager manages all your requests and workers
@@ -29,6 +34,15 @@ class MainActivity : AppCompatActivity() {
                 systemBars.bottom)
             insets
         }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
+        }
+
         //Create a constraint of which your workers are bound to.
         //Here the workers cannot execute the given process if
         //there's no internet connection
@@ -82,6 +96,7 @@ class MainActivity : AppCompatActivity() {
             .observe(this) { info ->
                 if (info.state.isFinished) {
                     showResult("Second process is done")
+                    launchNotificationService()
                 }
             }
     }
@@ -94,4 +109,27 @@ class MainActivity : AppCompatActivity() {
     private fun showResult(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
+
+    //Launch the NotificationService
+    private fun launchNotificationService() {
+        //Observe if the service process is done or not
+        //If it is, show a toast with the channel ID in it
+        NotificationService.trackingCompletion.observe(this) { id ->
+            showResult("Process for Notification Channel ID $id is done!")
+        }
+
+        //Create an Intent to start the NotificationService
+        //An ID of "001" is also passed as the notification channel ID
+        val serviceIntent = Intent(this,
+            NotificationService::class.java).apply {
+            putExtra(EXTRA_ID, "001")
+        }
+
+        //Start the foreground service through the Service Intent
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
+    companion object{
+        const val EXTRA_ID = "Id"
+    }
+
 }
